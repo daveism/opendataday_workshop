8537f537268aebc4c8dc1ead7b4a48e69b288a94
Dave Michelson <dmichelson@ashevillenc.gov>
2012-10-10 19:46:14 -0300

    Update Startup.sh

--- Startup.sh
+++ Startup.sh
@@ -9,10 +9,12 @@ export DJANGO_SETTINGS_MODULE=myblock.settings
 
 
 #replace the settings python script with the git hub copy the new python importer script from git hub repository
-sudo chmod 664 /home/openblock/openblock/src/myblock/myblock/settings.py
+sudo chmod 664 ~/opendataday_workshop/settings.py
 sudo chmod 664 ~/opendataday_workshop/Import_blocks_opendataday.py
 sudo mv /home/openblock/openblock/src/myblock/myblock/settings.py /home/openblock/openblock/src/myblock/myblock/settings_orig.py
-sudo cp ~/opendataday_workshop/Import_blocks_opendataday.py /home/openblock/openblock/src/myblock/myblock/settings.py
+sudo mv /home/openblock/openblock/src/openblock/ebpub/ebpub/streets/blockimport/esri/importers/blocks.py /home/openblock/openblock/src/openblock/ebpub/ebpub/streets/blockimport/esri/importers/blocks_orig.py
+sudo cp  ~/opendataday_workshop/settings.py /home/openblock/openblock/src/myblock/myblock/settings.py
+sudo cp ~/opendataday_workshop/Import_blocks_opendataday.py /home/openblock/openblock/src/openblock/ebpub/ebpub/streets/blockimport/esri/importers/blocks.py
 sudo chown openblock /home/openblock/openblock/src/myblock/myblock/settings.py
 
 sudo touch /home/openblock/openblock/wsgi/myblock.wsgi
