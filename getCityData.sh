80b087249935a0ea319416c90e1075c78a2ab544
Dave Michelson <dmichelson@ashevillenc.gov>
2012-10-12 11:40:47 -0300

    Update getCityData.sh

--- getCityData.sh
+++ getCityData.sh
@@ -45,6 +45,7 @@ ogrinfo  coa_development_locations_view.shp -sql "ALTER TABLE  coa_development_l
 ogrinfo  coa_development_locations_view.shp -sql "ALTER TABLE  coa_development_locations_view add column title character(150)"
 ogrinfo  coa_development_locations_view.shp -sql "ALTER TABLE  coa_development_locations_view add column item_date character(150)"
 ogrinfo  coa_development_locations_view.shp -sql "ALTER TABLE  coa_development_locations_view add column desc character(150)"
+ogrinfo  coa_development_locations_view.shp -sql "ALTER TABLE  coa_development_locations_view add column reason character(150)"
 
 
 
